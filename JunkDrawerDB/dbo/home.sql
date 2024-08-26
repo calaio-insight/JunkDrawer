7e763308187cc07a59ce82d5e0f021e9e403671b
scalaio <samantha.calaio@insight.com>
2024-08-26 10:32:16 -0400

    First table create

---
+++ JunkDrawerDB/dbo/home.sql
@@ -0,0 +1,17 @@
+﻿create table home
+(
+    homeId        int identity,
+    homePhoto     nvarchar(max),
+    address       nvarchar(250),
+    city          nvarchar(250),
+    state         nvarchar(2),
+    zip           nvarchar(50),
+    purchaseDate  datetime2,
+    purchasePrice decimal,
+    createdBy     nvarchar(max),
+    createdDate   datetime2,
+    modifiedBy    nvarchar(max),
+    modifiedDate  datetime2
+)
+go
+
